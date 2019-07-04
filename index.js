dc1832a6e90d32a033e93f58895dcc9e5f738193
Tejveer Singh <44260124+singhtej@users.noreply.github.com>
2019-07-04 17:38:51 +0200

    calling the logger

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-const Person  = require('./person');
-const person1 = new Person('tejveer', 27);
-person1.greeting();
+// const Person  = require('./person');
+// const person1 = new Person('tejveer', 27);
+// person1.greeting();
+
+
+const Logger = require('./logger');
+
+const logger = new Logger();
+
+logger.on('message', (data) => console.log('called Listener', data));
+
+logger.log('Hello World');
