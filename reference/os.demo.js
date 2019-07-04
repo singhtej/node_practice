2e38245c3d527d15d4abf3329caceab4283c0ec2
Tejveer Singh <44260124+singhtej@users.noreply.github.com>
2019-07-04 16:31:47 +0200

    more functions used

--- reference/os.demo.js
+++ reference/os.demo.js
@@ -8,3 +8,18 @@ console.log(os.arch());
 
 // CpU Core Info
 console.log(os.cpus());
+
+// Free Memory
+console.log(os.freemem());
+
+// Total Memory
+console.log(os.totalmem());
+
+// Home directory of the current user
+console.log(os.homedir());
+
+// Hostname of the operating system
+console.log(os.hostname);
+
+// system uptimr in numbers of seconds
+console.log(os.uptime());
